f82db88aaaa1ae225aacbbec78b7e4b22f016bbb
badmagick <uzairfarooqi@gmail.com>
2023-07-27 18:03:37 +0100

    refactor(kpopcomebacks): improve formatting

--- djangobackend/kpopcomebacks/utils.py
+++ djangobackend/kpopcomebacks/utils.py
@@ -1,13 +1,10 @@
-import random
-from datetime import datetime, timedelta
-from time import perf_counter
+from datetime import datetime
 
 from django.core.paginator import Paginator
 from django.db.models import Q
-from kpopcomebacks.models import Artist, Release, ReleaseType
+from kpopcomebacks.models import Release
 
-random.seed(42)
-PAGE_SIZE = 8
+PAGE_SIZE = 6
 
 
 def format_comebacks(comebacks: list[Release], page_number: str | None = None) -> dict:
@@ -21,25 +18,23 @@ def format_comebacks(comebacks: list[Release], page_number: str | None = None) -
         page_number = paginator.num_pages
     page = paginator.page(page_number)
     comebacks = [cb for cb in page]
-    page_numbers = list()
-    if page.has_previous():
-        page_numbers.append(page.previous_page_number())
-    page_numbers.append(page.number)
-    if page.has_next():
-        page_numbers.append(page.next_page_number())
-    if len(page_numbers) < 3 and paginator.num_pages > 2:
-        if page_numbers[0] == 1:
-            page_numbers.append(3)
-        else:
-            page_numbers.insert(0, page.previous_page_number() - 1)
+    prev = page.previous_page_number() if page.has_previous() else None
+    next_ = page.next_page_number() if page.has_next() else None
+    prev_jump = page.previous_page_number() - 3 if prev else None
+    if prev_jump and prev_jump < 1:
+        prev_jump = None
+    next_jump = page.next_page_number() + 3 if next_ else None
+    if next_jump and next_jump > paginator.num_pages:
+        next_jump = None
     page = {
         "current": page.number,
-        "previous": page.previous_page_number() if page.has_previous() else page.number,
-        "next": page.next_page_number() if page.has_next() else page.number,
+        "previous": prev,
+        "next": next_,
+        "previous_jump": prev_jump,
+        "next_jump": next_jump,
+        "first": 1 if page.number != 1 else None,
+        "last": paginator.num_pages if page.number != paginator.num_pages else None,
         "total": paginator.num_pages,
-        "has_next": page.has_next(),
-        "has_previous": page.has_previous(),
-        "page_numbers": page_numbers,
     }
     return {
         "comebacks": comebacks,
