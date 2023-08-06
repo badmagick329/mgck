ac0334b0c24b5563280d5ff62130b4fc963f2333
badmagick <uzairfarooqi@gmail.com>
2023-08-07 00:06:58 +0100

    feat(kpopcomebacks): add search to admin page

--- djangobackend/kpopcomebacks/admin.py
+++ djangobackend/kpopcomebacks/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from kpopcomebacks.models import Artist, Release, ReleaseType
 
-# Register your models here.
-admin.site.register(Artist)
-admin.site.register(Release)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("name", "id")
+    search_fields = ["name"]
+
+
+class ReleaseAdmin(admin.ModelAdmin):
+    list_display = ("artist", "album", "title", "release_date", "release_type")
+    search_fields = [
+        "artist__name",
+        "album",
+        "title",
+        "release_date",
+        "release_type__name",
+    ]
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Release, ReleaseAdmin)
 admin.site.register(ReleaseType)
