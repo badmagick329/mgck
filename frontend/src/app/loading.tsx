96624bbeaedb32b19e8d6256723ea0b3597e998b
badmagick <uzairfarooqi@gmail.com>
2024-03-01 19:35:14 +0000

    feat: move loader position

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -2,7 +2,7 @@ import { CgSpinnerTwo } from "react-icons/cg";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full">
+    <div className="flex flex-col items-center justify-center w-full min-h-screen">
       <CgSpinnerTwo className="animate-spin text-6xl" />
     </div>
   );
