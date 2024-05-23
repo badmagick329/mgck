bc0a5a6f13a02a8dd7ea7b49ef9f64e544fd8eed
badmagick329 <uzairfarooqi@gmail.com>
2024-05-23 21:38:04 +0100

    fix(kpopcomebacks): playlist urls for youtube player

--- frontend/src/app/kpop/_components/youtube-player.tsx
+++ frontend/src/app/kpop/_components/youtube-player.tsx
@@ -1,7 +1,8 @@
 export default function YoutubePlayer({ videoId }: { videoId: string }) {
+  const cleanId = videoId.split('?')[0].split('&')[0];
   return (
     <iframe
-      src={`https://www.youtube.com/embed/${videoId}`}
+      src={`https://www.youtube.com/embed/${cleanId}`}
       className='rounded-md border-0'
       allow='autoplay; encrypted-media'
       allowFullScreen
