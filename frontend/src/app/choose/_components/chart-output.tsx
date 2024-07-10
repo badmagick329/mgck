8fa40ae6238791325a68bfe66063690f0887368d
badmagick329 <uzairfarooqi@gmail.com>
2024-07-10 18:17:04 +0100

    feat(choose): improve chart layout

--- frontend/src/app/choose/_components/chart-output.tsx
+++ frontend/src/app/choose/_components/chart-output.tsx
@@ -1,6 +1,13 @@
 'use client';
 
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
@@ -22,29 +29,37 @@ export default function ChartOutput({ chartData }: { chartData: Result[] }) {
   }
 
   return (
-    <ChartContainer
-      config={chartConfig}
-      className='min-h-[200px] w-full md:max-w-[720px]'
-    >
-      <BarChart accessibilityLayer data={chartData}>
-        <CartesianGrid vertical={false} />
-        <XAxis
-          dataKey='choice'
-          tickLine={false}
-          tickMargin={10}
-          axisLine={true}
-          tickFormatter={(value) => value.slice(0, 4)}
-        />
-        <YAxis ticks={[0, 25, 50, 75, 100]} />
-        <ChartTooltip content={<ChartTooltipContent />} />
-        <Bar
-          dataKey='score'
-          fill='var(--color-score)'
-          radius={2}
-          label={{ fill: 'white', fontSize: 16 }}
-          barSize={48}
-        />
-      </BarChart>
-    </ChartContainer>
+    <Card className='flex w-full flex-col items-center'>
+      <CardHeader className='flex items-center justify-center'>
+        <CardTitle>Choice Comparison</CardTitle>
+        <CardDescription>Winner: {chartData[0].choice}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer
+          config={chartConfig}
+          className='min-h-96 w-full md:max-w-[720px]'
+        >
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey='choice'
+              tickLine={false}
+              tickMargin={10}
+              axisLine={true}
+              tickFormatter={(value) => value.slice(0, 4)}
+            />
+            <YAxis ticks={[0, 25, 50, 75, 100]} />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Bar
+              dataKey='score'
+              fill='var(--color-score)'
+              radius={2}
+              label={{ fill: 'white', fontSize: 12 }}
+              barSize={48}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
   );
 }
