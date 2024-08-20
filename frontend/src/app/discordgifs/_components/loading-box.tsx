e19572b021a58fe94c37efe162ddf368a4b676cc
badmagick329 <uzairfarooqi@gmail.com>
2024-08-21 00:48:46 +0100

    feat(discordgifs): randomise loading box speed

--- frontend/src/app/discordgifs/_components/loading-box.tsx
+++ frontend/src/app/discordgifs/_components/loading-box.tsx
@@ -13,7 +13,11 @@ export default function LoadingBox(props: JSX.IntrinsicElements['mesh']) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   const [currentDir, setCurrentDir] = useState<Vector3>(
-    new Vector3(getSpeed(), getSpeed(), 0)
+    new Vector3(
+      getSpeed() * (Math.random() > 0.5 ? 1 : -1),
+      getSpeed() * (Math.random() > 0.5 ? 1 : -1),
+      0
+    )
   );
   useEffect(() => {
     if (meshRef.current) {
@@ -48,19 +52,19 @@ export default function LoadingBox(props: JSX.IntrinsicElements['mesh']) {
 }
 
 function getX(currentX: number, currentDir: Vector3) {
-  if (currentX >= 6) {
-    return getSpeed() * -1;
-  } else if (currentX <= -6) {
-    return getSpeed();
+  if (currentX >= 5.3) {
+    return Math.abs(currentDir.x) * -1;
+  } else if (currentX <= -5.3) {
+    return Math.abs(currentDir.x);
   }
   return currentDir.x;
 }
 
 function getY(currentY: number, currentDir: Vector3) {
   if (currentY >= 3) {
-    return getSpeed() * -1;
+    return Math.abs(currentDir.y) * -1;
   } else if (currentY <= -3) {
-    return getSpeed();
+    return Math.abs(currentDir.y);
   }
   return currentDir.y;
 }
