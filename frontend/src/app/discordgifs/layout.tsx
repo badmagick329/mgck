856938c14b2b601d4068cecce986653c19b62022
badmagick329 <uzairfarooqi@gmail.com>
2024-08-22 01:04:07 +0100

    feat(discordgifs): add metadata

--- frontend/src/app/discordgifs/layout.tsx
+++ frontend/src/app/discordgifs/layout.tsx
@@ -3,6 +3,18 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Create emotes and stickers',
+  description:
+    'Use your video clips to make discord emotes and stickers that fit the size limits',
+  keywords: [
+    'discord',
+    'emotes',
+    'stickers',
+    'video',
+    'clips',
+    'gifs',
+    'animated',
+    'emojis',
+  ],
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
