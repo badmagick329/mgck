32f4062d9ec83d4d8158a749aa9975cac6d2bb85
badmagick329 <uzairfarooqi@gmail.com>
2024-08-24 01:41:30 +0100

    fix(gfys): clear empty search params

--- frontend/src/lib/utils/gfys.ts
+++ frontend/src/lib/utils/gfys.ts
@@ -1,10 +1,10 @@
 import {
   GfyData,
-  GfyResult,
-  GfyResponse,
   GfyParsedResponse,
-} from "@/lib/types";
-import { ReadonlyURLSearchParams } from "next/navigation";
+  GfyResponse,
+  GfyResult,
+} from '@/lib/types';
+import { ReadonlyURLSearchParams } from 'next/navigation';
 
 export function parseGfyResponse(resp: GfyResponse): GfyParsedResponse {
   const newData: Array<GfyData> = [];
@@ -25,7 +25,7 @@ export function parseGfyResponse(resp: GfyResponse): GfyParsedResponse {
     previous: resp.previous,
     totalPages: resp.total_pages,
     gfys: newData,
-  }
+  };
 }
 
 export function imgurIdToMp4(imgurId: string) {
@@ -40,21 +40,17 @@ export function imgurIdToUrl(imgurId: string) {
   return `https://i.imgur.com/${imgurId}`;
 }
 
-
 export function cleanedSearchParams(urlSearchParams: URLSearchParams) {
-  if (urlSearchParams.get("title")?.trim() == "") {
-    urlSearchParams.delete("title");
+  const params = ['title', 'tags', 'account', 'start_date', 'end_date', 'page'];
+  for (const param of params) {
+    if (urlSearchParams.get(param)?.trim() === '') {
+      urlSearchParams.delete(param);
+    }
   }
-  if (urlSearchParams.get("tags")?.trim() == "") {
-    urlSearchParams.delete("tags");
-  }
-  if (urlSearchParams.get("account")?.trim() == "") {
-    urlSearchParams.delete("account");
-  }
-  const pageParam = urlSearchParams.get("page")?.trim() || "";
+  const pageParam = urlSearchParams.get('page')?.trim() || '';
   const page = parseInt(pageParam);
   if (page < 1 || isNaN(page)) {
-    urlSearchParams.delete("page");
+    urlSearchParams.delete('page');
   }
   return urlSearchParams;
 }
@@ -66,7 +62,7 @@ export function createURL(
 ) {
   const urlSearchParams = new URLSearchParams(paramsString);
   if (asPage) {
-    urlSearchParams.set("page", asPage.toString());
+    urlSearchParams.set('page', asPage.toString());
   }
   const cleanParams = cleanedSearchParams(urlSearchParams);
   const newParams = cleanParams.toString();
@@ -80,25 +76,24 @@ export function formDataFromSearchParams(
   searchParams: ReadonlyURLSearchParams | URLSearchParams
 ) {
   const formData = new FormData();
-  const titleParam = (searchParams.get("title") || "") as string;
-  const tagsParam = (searchParams.get("tags") || "") as string;
-  const pageParam = (searchParams.get("page") || "1") as string;
-  const accountParam = (searchParams.get("account") || "") as string;
-  const startDateParam = (searchParams.get("start_date") || "") as string;
-  const endDateParam = (searchParams.get("end_date") || "") as string;
-  formData.append("title", titleParam);
-  formData.append("tags", tagsParam);
-  formData.append("page", pageParam);
-  formData.append("account", accountParam);
-  formData.append("start_date", startDateParam);
-  formData.append("end_date", endDateParam);
+  const titleParam = (searchParams.get('title') || '') as string;
+  const tagsParam = (searchParams.get('tags') || '') as string;
+  const pageParam = (searchParams.get('page') || '1') as string;
+  const accountParam = (searchParams.get('account') || '') as string;
+  const startDateParam = (searchParams.get('start_date') || '') as string;
+  const endDateParam = (searchParams.get('end_date') || '') as string;
+  formData.append('title', titleParam);
+  formData.append('tags', tagsParam);
+  formData.append('page', pageParam);
+  formData.append('account', accountParam);
+  formData.append('start_date', startDateParam);
+  formData.append('end_date', endDateParam);
   return formData;
 }
 
-
 export function formDataFromSearchParamsString(searchParamsString: string) {
   let cleanedParams = searchParamsString;
-  if (cleanedParams[0] == "/") {
+  if (cleanedParams[0] == '/') {
     cleanedParams = cleanedParams.slice(1);
   }
   const urlSearchParams = new URLSearchParams(cleanedParams);
