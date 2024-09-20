c81db62a3d19d708b5bab0fac2d0b9edcab88dd5
badmagick329 <uzairfarooqi@gmail.com>
2024-09-20 21:09:31 +0100

    feat: navbar link and layout

--- frontend/src/app/gfys/page.tsx
+++ frontend/src/app/gfys/page.tsx
@@ -4,9 +4,9 @@ import GfyList from './_components/gfy-list';
 import SearchForm from './_components/search-form';
 import SearchNavigation from './_components/search-navigation';
 
-export default function Home() {
+export default function GfysHome() {
   return (
-    <main className='bg-background-gf dark:bg-background-gf-dark flex min-h-dvh flex-col items-center'>
+    <main className='bg-background-gf dark:bg-background-gf-dark flex min-h-dvh w-full flex-col items-center'>
       <Navbar />
       <div className='flex w-full grow flex-col items-center px-10 py-4'>
         <SearchForm />
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,8 +1,27 @@
+'use client';
+
 import { ThemeToggler } from '@/components/theme-toggler';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { IoHome } from 'react-icons/io5';
+
+import { Button } from './ui/button';
 
 export default function Navbar() {
+  const path = usePathname();
+  const isHome = path === '/';
   return (
-    <div className='ml-auto p-2'>
+    <div className='flex w-full justify-between p-2'>
+      <Link href='/'>
+        <Button
+          className='text-xl'
+          size='icon'
+          variant='outline'
+          disabled={isHome}
+        >
+          <IoHome />
+        </Button>
+      </Link>
       <ThemeToggler />
     </div>
   );
