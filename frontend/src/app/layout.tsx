d70298a7ea8183346725eedfd884358015ed5893
badmagick329 <uzairfarooqi@gmail.com>
2024-10-22 19:04:27 +0100

    feat: analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
+import Script from 'next/script';
 
 import './globals.css';
 
@@ -20,6 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' suppressHydrationWarning>
+      <head>
+        <Script
+          src='https://analytics.mgck.ink/js/script.js'
+          data-domain='kristalomu.com'
+          strategy='afterInteractive'
+        />
+      </head>
       <body className={`${font.className} tracking-wide`}>
         <ThemeProvider
           attribute='class'
