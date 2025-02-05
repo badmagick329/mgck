80e488d6cb3cbdd0e4070f94ebdf1442e69a314c
badmagick329 <uzairfarooqi@gmail.com>
2025-02-05 13:26:59 +0000

    refactor

--- frontend/src/app/gfys/_components/search-text-input.tsx
+++ frontend/src/app/gfys/_components/search-text-input.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import {
   Tooltip,
   TooltipContent,
@@ -35,7 +34,7 @@ export default function SearchTextInput({
       <Tooltip>
         <TooltipTrigger asChild>
           <input
-            className='bg-background-gf-dark/15 dark:bg-background-gf-dark flex h-10 w-[12rem] rounded-md border-2 border-black/60 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white/60'
+            className='flex h-10 w-[12rem] rounded-md border-2 border-black/60 bg-background-gf-dark/15 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white/60 dark:bg-background-gf-dark'
             name={name}
             placeholder={placeholder}
             autoComplete='off'
@@ -48,21 +47,6 @@ export default function SearchTextInput({
         </TooltipTrigger>
         <TooltipContent>
           <p>{tooltip}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  );
-}
-
-export function TooltipDemo() {
-  return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button variant='outline'>Hover</Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Add to library</p>
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
