0194d841c2d57f2819d8982c67497e5bfa629bea
badmagick329 <uzairfarooqi@gmail.com>
2025-02-26 21:11:03 +0000

    feat(account): add feature list for accepted users

--- frontend/src/app/account/home/_components/AcceptedUserHome.tsx
+++ frontend/src/app/account/home/_components/AcceptedUserHome.tsx
@@ -1,16 +1,23 @@
 import { UserHomeProps } from '@/app/account/home/page';
 import LogoutButton from './LogoutButton';
+import Link from 'next/link';
 
 export default async function AcceptedUserHome({ username }: UserHomeProps) {
-  const content =
-    'You have been accepted. You can now access.. something?? soon??? ™️';
+  const content = 'You have been accepted. You can now access the following';
   return (
-    <main className={'flex flex-col min-h-screen items-center gap-2'}>
+    <main className={'flex flex-col min-h-screen items-center gap-4'}>
       <h1 className={'text-4xl font-bold pt-6'}>
         Account Status for {username}
       </h1>
       <LogoutButton />
       <p className={'text-lg text-green-300'}>{content}</p>
+      <ul className='pt-4'>
+        <li>
+          <Link className='text-lg hover:underline' href='/emojify'>
+            AI Generation on the emojifier
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
