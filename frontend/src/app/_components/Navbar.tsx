bfc0040e1b94126b5f276d70e4f837b274febf4b
badmagick329 <uzairfarooqi@gmail.com>
2025-03-08 22:17:12 +0000

    feat(account): add account page link to navbar

--- frontend/src/app/_components/Navbar.tsx
+++ frontend/src/app/_components/Navbar.tsx
@@ -4,24 +4,45 @@ import { ThemeToggler } from '@/app/_components/ThemeToggler';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { IoHome } from 'react-icons/io5';
+import { FaUser } from 'react-icons/fa';
 
 import { Button } from '@/components/ui/button';
 
 export default function Navbar() {
   const path = usePathname();
   const isHome = path === '/';
+  const isUserHome = path.startsWith('/account');
+
   return (
     <div className='flex w-full justify-between p-2'>
-      <Link href='/'>
-        <Button
-          className='text-xl'
-          size='icon'
-          variant='outline'
-          disabled={isHome}
+      <div className='flex gap-2'>
+        <Link
+          className={`${isHome ? 'cursor-default' : 'cursor-pointer'}`}
+          href='/'
         >
-          <IoHome />
-        </Button>
-      </Link>
+          <Button
+            className='text-xl'
+            size='icon'
+            variant='outline'
+            disabled={isHome}
+          >
+            <IoHome />
+          </Button>
+        </Link>
+        <Link
+          className={`${isUserHome ? 'cursor-default' : 'cursor-pointer'}`}
+          href='/account'
+        >
+          <Button
+            className='text-xl'
+            size='icon'
+            variant='outline'
+            disabled={isUserHome}
+          >
+            <FaUser />
+          </Button>
+        </Link>
+      </div>
       <ThemeToggler />
     </div>
   );
