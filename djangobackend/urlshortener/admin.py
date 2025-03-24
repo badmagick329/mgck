0ea312c8a7281aa5abc0916d2346e4ddf410aaba
badmagick329 <uzairfarooqi@gmail.com>
2025-03-24 18:53:43 +0000

    feat(shortener): import admin dashboard

--- djangobackend/urlshortener/admin.py
+++ djangobackend/urlshortener/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+
 from urlshortener.models import ShortURL
 
-admin.site.register(ShortURL)
+
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = (
+        "url",
+        "short_id",
+        "created",
+        "created_by",
+        "accessed",
+        "number_of_uses",
+        "redirect_url",
+    )
+    search_fields = [
+        "url",
+        "short_id",
+        "created",
+        "created_by",
+    ]
+
+
+admin.site.register(ShortURL, ShortURLAdmin)
