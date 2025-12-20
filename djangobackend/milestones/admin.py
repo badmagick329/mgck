d7e06a721bd76cc4b2ab147b10a813b23dfedb6e
badmagick329 <uzairfarooqi@gmail.com>
2025-12-20 21:02:17 +0000

    feat: register models for admin view

--- djangobackend/milestones/admin.py
+++ djangobackend/milestones/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from milestones.models import Milestone, MilestoneUser
+
+
+class MilestoneAdmin(admin.ModelAdmin):
+    list_display = (
+        "event_name",
+        "event_datetime_utc",
+        "event_timezone",
+        "created",
+        "created_by",
+    )
+    search_fields = [
+        "created_by",
+        "event_name",
+        "event_datetime_utc",
+        "event_timezone",
+    ]
+
+
+class MilestoneUserAdmin(admin.ModelAdmin):
+    list_display = ("username",)
+    search_fields = [
+        "username",
+    ]
+
+
+admin.site.register(Milestone, MilestoneAdmin)
+admin.site.register(MilestoneUser, MilestoneUserAdmin)
