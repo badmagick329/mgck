165b8380e198ae2e1125bc190f7765be41fa6bc6
badmagick329 <uzairfarooqi@gmail.com>
2025-12-25 13:26:37 +0000

    feat: add more validation on the zod schemas

--- frontend/src/lib/types/milestones.ts
+++ frontend/src/lib/types/milestones.ts
@@ -2,25 +2,35 @@ import { z } from 'zod';
 
 export const serverMilestoneSchema = z.object({
   id: z.number(),
-  event_name: z.string().trim().nonempty(),
+  event_name: z.string().trim().nonempty().max(255),
   event_datetime_utc: z.string().trim().nonempty(),
-  event_timezone: z.string().trim().nonempty(),
+  event_timezone: z.string().trim().nonempty().max(63),
   created: z.string().trim().nonempty(),
-  color: z.string().refine((s) => s.length === 7 || s.length === 4, {
-    message: 'Color must be a valid hex code of length 7 or 4',
-  }),
+  color: z
+    .string()
+    .refine((s) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s), {
+      message: 'Color must be a valid hex code like #RRGGBB or #RGB',
+    }),
 });
 export type ServerMilestone = z.infer<typeof serverMilestoneSchema>;
 
 export const serverMilestoneListSchema = z.array(serverMilestoneSchema);
 
 export const clientMilestoneSchema = z.object({
-  name: z.string().trim().nonempty({ message: 'Name cannot be empty' }),
+  name: z
+    .string()
+    .trim()
+    .nonempty({ message: 'Name cannot be empty' })
+    .max(255, {
+      message: 'Name cannot exceed 255 characters',
+    }),
   timestamp: z.number(),
-  timezone: z.string(),
-  color: z.string().refine((s) => s.length === 7 || s.length === 4, {
-    message: 'Color must be a valid hex code of length 7 or 4',
-  }),
+  timezone: z.string().nonempty().max(63),
+  color: z
+    .string()
+    .refine((s) => /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(s), {
+      message: 'Color must be a valid hex code like #RRGGBB or #RGB',
+    }),
 });
 export type ClientMilestone = z.infer<typeof clientMilestoneSchema>;
 
