ca1e21c752fe9b003d3d2d39f54ed7407d1d3942
badmagick329 <uzairfarooqi@gmail.com>
2025-12-26 20:40:39 +0000

    refactor

--- frontend/src/app/milestones/_components/MilestoneActions.tsx
+++ frontend/src/app/milestones/_components/MilestoneActions.tsx
@@ -9,15 +9,7 @@ import { EllipsisVerticalIcon, EyeIcon, EyeOffIcon } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 
