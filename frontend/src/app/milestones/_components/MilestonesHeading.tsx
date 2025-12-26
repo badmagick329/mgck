761d9629052e6e4a8771c431c93f360500a142b5
badmagick329 <uzairfarooqi@gmail.com>
2025-12-26 17:41:02 +0000

    feat: add heading animation

--- frontend/src/app/milestones/_components/MilestonesHeading.tsx
+++ frontend/src/app/milestones/_components/MilestonesHeading.tsx
@@ -1,7 +1,28 @@
+import SignPostSvg from '@/app/milestones/_components/SignPostSvg';
+import { motion } from 'motion/react';
+import { Outfit } from 'next/font/google';
+
+const font = Outfit({
+  subsets: ['latin'],
+  weight: ['700'],
+});
+
 export default function MilestonesHeading() {
   return (
-    <h1 className='mx-auto text-3xl font-bold underline underline-offset-4'>
-      Milestones
-    </h1>
+    <div className={`flex justify-center gap-2 ${font.className}`}>
+      <SignPostSvg />
+      <motion.h1
+        initial={{ x: 100, opacity: 0.5 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 300,
+          damping: 15,
+        }}
+        className='mt-auto pt-1 text-center text-4xl font-bold'
+      >
+        Milestones
+      </motion.h1>
+    </div>
   );
 }
