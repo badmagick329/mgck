ea97ecc4b32c5bedd86076aea1b3aac9470670f4
badmagick329 <uzairfarooqi@gmail.com>
2026-01-01 15:21:45 +0000

    feat: change milestones description

--- frontend/src/app/_components/MilestonesIntro.tsx
+++ frontend/src/app/_components/MilestonesIntro.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function MilestonesIntro() {
   return (
     <div className='grid place-content-center place-items-center gap-8 border-t-2 border-primary-ml bg-background-ml px-8 py-4 sm:px-12 sm:py-6'>
-      <span className='text-lg'>Make every day count</span>
+      <span className='text-lg'>Milestone in minutes, days, seconds.</span>
       <Link href={MILESTONES_BASE}>
         <Button className='shadow-glow-primary-ml bg-primary-ml/70 w-72 font-semibold text-primary-foreground hover:bg-primary-ml md:w-96 md:text-lg'>
           Milestones
