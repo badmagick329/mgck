4afb7dac34c44c4b36026b7e9d40a8ca070c2365
badmagick329 <uzairfarooqi@gmail.com>
2026-01-01 15:16:58 +0000

    feat: improve secondary button formatting

--- frontend/src/components/ui/MilestonesButton.tsx
+++ frontend/src/components/ui/MilestonesButton.tsx
@@ -5,7 +5,7 @@ const appVariants = {
   milestonesPrimary:
     'bg-primary-ml hover:bg-primary-ml hover:font-bold hover:scale-105',
   milestonesSecondary:
-    'bg-secondary-ml hover:bg-secondary-ml-hover hover:text-secondary-ml-foreground hover:text-sceondary-foreground-ml',
+    'bg-primary-ml/70 hover:bg-primary-ml hover:text-primary-ml-foreground hover:text-primary-foreground-ml text-xs sm:text-sm px-2 py-1 sm:px-4 sm:py-2',
 } as const;
 
 interface AppButtonProps extends ButtonProps {
